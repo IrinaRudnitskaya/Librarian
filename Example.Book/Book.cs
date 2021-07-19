b339f32d8bd51c1868d35752ce4253dc909bab52
IrinaRudnitskaya <irina.m.rudnitsk@gmail.com>
2021-07-19 12:16:01 +0300

    Добавьте файлы проекта.

---
+++ Example.Book/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example.Book
+{
+    public class Book
+    {
+        int width;
+        int height;
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public int Lengthwise { get; set; }
+        public int NumberOfPages { get; set; } = 100;
+        public int NumberOfPagesRead { get; set; }
+        public int GetNumberOfPagesUnread()
+        {
+            return NumberOfPages - NumberOfPagesRead;
+        }
+        public int 
+    }
+}
---
+++ Example.Book/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example.Book
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book myFirstBook = new Book();
+
+            myFirstBook.Height = 10;
+            myFirstBook.Width = 20;
+            myFirstBook.Lengthwise = myFirstBook.Width;
+            myFirstBook.NumberOfPagesRead = 37;
+            int UnreadPages = myFirstBook.GetNumberOfPagesUnread();
+
+            Console.WriteLine("Высота: " + myFirstBook.Height);
+            Console.WriteLine("Ширина: " + myFirstBook.Width);
+            Console.WriteLine("Толщина: " + myFirstBook.Lengthwise);
+            Console.WriteLine("Количество страниц: " + myFirstBook.NumberOfPages);
+            Console.WriteLine("Количество прочитанных страниц: " + myFirstBook.NumberOfPagesRead);
+            Console.WriteLine("Количество непрочитанных страниц: " + UnreadPages);
+            Console.ReadLine();
+        }       
+    }
+}
